f8aefa8cd1800fce7db3fe36539321a689f8ddb1
lhkwok9 <s2007klhwaiommk@gmail.com>
2023-06-06 23:26:06 +0800

    Get the arm camera

--- try/quickStart.py
+++ try/quickStart.py
@@ -4,7 +4,7 @@ import robosuite as suite
 # create environment instance
 env = suite.make(
     env_name="Lift", # try with other tasks like "Stack" and "Door"
-    robots="Panda", # try with other robots like "Sawyer" and "Jaco"
+    robots="Baxter", # try with other robots like "Sawyer" and "Jaco"
     has_renderer=True,
     has_offscreen_renderer=False,
     use_camera_obs=False,
